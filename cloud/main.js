3a0cda8bac83b93dcb27229459904f51c543e5a8
John-Paul Anyiam-Osigwe <johnpaulanyiam@gmail.com>
2016-08-21 19:18:21 -0400

    new

--- cloud/main.js
+++ cloud/main.js
@@ -10,27 +10,27 @@ Parse.Cloud.define("chargePTGUser", function (request, response) {
     headers: {
         'Authorization': 'Basic c2tfdGVzdF9Qdkx5NjBpRUZtQ0pyWXFyb3FmUkIxam0'
       },
-    // var data = request.params["data"];
-    // var type = request.params["type"];
-    // var accountId = request.params["user_id"];
-
-    // var card = data.object.source.id;
-    // var cardLast4 = data.object.source.last4;
-    // var amount = data.object.amount;
-    // var typeId = data.object.id;
-    // var customerId = data.object.customer;
-    // var objectName = data.object.object;
 
     success: function(httpResponse) {
+        var data = httpResponse["data"];
+        var type = httpResponse["type"];
+        var accountId = httpResponse["user_id"];
+
+        var card = data.object.source.id;
+        var cardLast4 = data.object.source.last4;
+        var amount = data.object.amount;
+        var typeId = data.object.id;
+        var customerId = data.object.customer;
+        var objectName = data.object.object;
         var eventObject = new Parse.Object("WebhookEvents");
-        eventObject.set("customerId", 'customerId');
-        // eventObject.set("accountId", accountId);
-        // eventObject.set("amount", amount);
-        // eventObject.set("type", type);
-        // eventObject.set("card", card);
-        // eventObject.set("cardLast4", cardLast4);
-        // eventObject.set("objectName", objectName);
-        // eventObject.set("typeId", typeId);
+        eventObject.set("customerId", customerId);
+        eventObject.set("accountId", accountId);
+        eventObject.set("amount", amount);
+        eventObject.set("type", type);
+        eventObject.set("card", card);
+        eventObject.set("cardLast4", cardLast4);
+        eventObject.set("objectName", objectName);
+        eventObject.set("typeId", typeId);
         eventObject.save(null, {
           success: function(eventObject) {
             response.success('** WEBHOOK WORKING **' + eventObject.id);
