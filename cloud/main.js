70cd4c7b9b11792cb40de0d970cd370ff8aa98ca
John-Paul Anyiam-Osigwe <johnpaulanyiam@gmail.com>
2016-08-23 22:23:40 -0400

    new

--- cloud/main.js
+++ cloud/main.js
@@ -5,6 +5,7 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.afterSave("SinchMessage", function(request) {
   // Our "Comment" class has a "text" key with the body of the comment itself
+  Parse.Cloud.useMasterKey();
   var messageText = request.object.get('text');
   var messageRecipient = request.object.get('recipientId');
   var messageSender = request.object.get('senderId');
@@ -27,8 +28,9 @@ Parse.Cloud.afterSave("SinchMessage", function(request) {
     },
     error: function(error) {
       throw "Got an error " + error.code + " : " + error.message;
-    }
-  },{ useMasterKey: true });
+    },
+    useMasterKey: true
+  });
 });
 
 Parse.Cloud.define("chargePTGUser", function (request, response) {
